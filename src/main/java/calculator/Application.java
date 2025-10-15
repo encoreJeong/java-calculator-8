9d58773cfc94e6299c60fd9ffbe02a55baf2be4b
encoreJeong <zzaibean@gmail.com>
2025-10-15 20:28:28 +0900

    feat: 기본기능구현 1. 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = Console.readLine();
+
+        String[] splitedInput = input.split("[,:]");
+
+        int inputSize = splitedInput.length;
+
+        Integer[] parsedInput = new Integer[inputSize];
+
+        for(int i = 0; i < splitedInput.length; i++) {
+            parsedInput[i] = Integer.parseInt(splitedInput[i]);
+        }
+
+        int sum = 0;
+        for(int i = 0; i < inputSize; i++) {
+            sum += parsedInput[i];
+        }
+
+        System.out.print("결과 : " + sum);
     }
 }
