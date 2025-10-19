3c641b73496cefbd78bb5a6b1b2e5df7c1dbc150
encoreJeong <zzaibean@gmail.com>
2025-10-19 22:20:16 +0900

    refactor: 기본기능 예외처리 추가 1. 입력이 주어지지 않는 경우, 정상 처리.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,21 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.NoSuchElementException;
+
 public class Application {
 
     public static void main(String[] args) {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
+
+        String input;
+
+        try {
+            input = Console.readLine();
+        } catch (NoSuchElementException e) {
+            input = "";
+        }
 
         StringExpression stringExpression = StringExpression.valueOf(input);
         Calculator calculator = new Calculator();
