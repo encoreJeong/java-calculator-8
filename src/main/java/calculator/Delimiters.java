43d56504f1593cf3a2e428a9a0b6489e025ca534
encoreJeong <zzaibean@gmail.com>
2025-10-19 22:04:11 +0900

    refactor: Delimiters 리펙토링 1. 상수 추가, 매직넘버 삭제 2. Delimiter 클래스에 유효한 구분자인지 확인하는 책임을 위임

--- src/main/java/calculator/Delimiters.java
+++ src/main/java/calculator/Delimiters.java
@@ -3,48 +3,57 @@ package calculator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Delimiters {
 
-    public static String HEADER_OPEN_TAG = "//";
-    public static String HEADER_CLOSE_TAG = "\\\\n";
+    public static final String EMPTY = "";
+    public static final String ESCAPE = "\\";
+    public static final String HEADER_OPEN_TAG = "//";
+    public static final String HEADER_CLOSE_TAG = "\\n";
+    private static final List<Delimiter> DEFAULT_DELIMITERS = Arrays.asList(
+                                                                Delimiter.valueOf(",").get(),
+                                                                Delimiter.valueOf(":").get());
 
-    private List<Character> delimiters;
+    private final List<Delimiter> delimiters;
 
-    private Delimiters(List<Character> delimiters) {
+    private Delimiters(List<Delimiter> delimiters) {
         this.delimiters = delimiters;
     }
 
     public static Delimiters from(String rawInput) {
 
-        List<Character> delimiters = new ArrayList<>(Arrays.asList(',',':'));
+        List<Delimiter> delimiters = new ArrayList<>(DEFAULT_DELIMITERS);
 
-        //입력의 맨 앞부분에 "//"와 "\n" 사이에 커스텀 구분자로 사용할 문자를 입력할 수 있다.
-        if(rawInput.startsWith(HEADER_OPEN_TAG)) {
-            if (!rawInput.contains(HEADER_CLOSE_TAG))
-                throw new IllegalArgumentException("커스텀 구분자 지정 형식이 잘못 됐습니다." + rawInput);
-
-            String customDelimiter = rawInput.substring(rawInput.indexOf(HEADER_OPEN_TAG) + HEADER_OPEN_TAG.length(), rawInput.indexOf(HEADER_CLOSE_TAG));
-            if (customDelimiter.length() > 1)
-                throw new IllegalArgumentException("커스텀 구분자 지정 형식이 잘못 됐습니다." + customDelimiter);
-
-            char verifiedCustomDelimiter = customDelimiter.charAt(0);
-
-            delimiters.add(verifiedCustomDelimiter);
-        }
+        getCustomDelimiter(rawInput).ifPresent(delimiters::add);
 
         return new Delimiters(delimiters);
     }
 
+    private static Optional<Delimiter> getCustomDelimiter(String rawInput) {
+        String delimiter = extractContentBetweenHeaderTags(rawInput);
+        return Delimiter.valueOf(delimiter);
+    }
+
+    private static String extractContentBetweenHeaderTags(String rawInput) {
+        validateHeaderFormat(rawInput);
+
+        if(rawInput.startsWith(HEADER_OPEN_TAG)) {
+            return rawInput.substring(rawInput.indexOf(HEADER_OPEN_TAG) + HEADER_OPEN_TAG.length(), rawInput.indexOf(HEADER_CLOSE_TAG));
+        }
+
+        return EMPTY;
+    }
+
     public String toRegExp() {
-        StringBuilder delimitersRegx = new StringBuilder("[,:");
+        StringBuilder delimitersRegx = new StringBuilder("[");
 
         delimiters.forEach(delimiter -> {
             //커스텀 구분자가 알파벳이거나 숫자라면 정규식 표현상에서 이스케이프 문자를 앞에 붙이면 안된다.
-            if(!Character.isLetterOrDigit(delimiter) ) {
-                delimitersRegx.append("\\");
+            if(!delimiter.isLetterOrDigit()) {
+                delimitersRegx.append(ESCAPE);
             }
-            delimitersRegx.append(delimiter);
+            delimitersRegx.append(delimiter.toCharacter());
         });
         delimitersRegx.append("]");
 
@@ -52,6 +61,21 @@ public class Delimiters {
     }
 
     public boolean hasCustomDelimiter() {
-        return delimiters.size() > 2;
+        return delimiters.size() > DEFAULT_DELIMITERS.size();
+    }
+
+    private static void validateHeaderFormat(String rawInput) {
+        if(isContainsHeaderOpenTag(rawInput)) {
+            validateHeaderContainsCloseTag(rawInput);
+        }
+    }
+
+    private static boolean isContainsHeaderOpenTag(String rawInput) {
+        return rawInput.startsWith(HEADER_OPEN_TAG);
+    }
+
+    private static void validateHeaderContainsCloseTag(String rawInput) {
+        if (!rawInput.contains(HEADER_CLOSE_TAG))
+            throw new IllegalArgumentException("커스텀 구분자 지정 형식이 잘못 됐습니다." + rawInput);
     }
 }
