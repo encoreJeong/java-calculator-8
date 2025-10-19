2635d61a253931b5181136bfa1c208fe656255dc
encoreJeong <zzaibean@gmail.com>
2025-10-19 22:59:29 +0900

    refactor: 문자열 입력/출력 책임 분리 1. StringExpressionIOUtils 에 문자열 입력, 출력 책임 위임

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,25 +1,14 @@
 package calculator;
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.NoSuchElementException;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-
-        String input;
-
-        try {
-            input = Console.readLine();
-        } catch (NoSuchElementException e) {
-            input = "";
-        }
+        String input = StringExpressionIOUtils.readLineWithHint();
 
         StringExpression stringExpression = StringExpression.valueOf(input);
         Calculator calculator = new Calculator();
 
-        System.out.print("결과 : " + calculator.calculate(stringExpression));
+        StringExpressionIOUtils.printWithHint(calculator.calculate(stringExpression));
     }
 }
---
+++ src/main/java/calculator/StringExpressionIOUtils.java
@@ -0,0 +1,29 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.NoSuchElementException;
+
+public class StringExpressionIOUtils {
+
+    private static final String INPUT_HINT = "덧셈할 문자열을 입력해 주세요.";
+    private static final String RESULT_HINT = "결과 : ";
+
+    public static String readLineWithHint() {
+        System.out.println(INPUT_HINT);
+
+        String input;
+
+        try {
+            input = Console.readLine();
+        } catch (NoSuchElementException e) {
+            input = "";
+        }
+
+        return input;
+    }
+
+    public static void printWithHint(Object obj) {
+        System.out.print(RESULT_HINT + obj);
+    }
+}
